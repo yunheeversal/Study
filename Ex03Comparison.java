0a728bf00d25170bb0728cd397dc684e284f5804
yunheeversal <98575048+yunheeversal@users.noreply.github.com>
2022-03-08 13:11:42 +0900

    Comparison add while
    while문 추가 한 비교

--- Ex03Comparison.java
+++ Ex03Comparison.java
@@ -12,9 +12,24 @@ public class Ex03Comparison {
        System.out.printf(">");
        int a = scanner.nextInt();
        
+       while (a < 0 || a > 100) {
+           System.out.println("잘못 입력했습니다.");
+           System.out.println("A 값을 입력하세요.");
+           System.out.print("> ");
+           a = scanner.nextInt();
+       }
+       
+       
        System.out.println("B 값을 입력하세요.");
        System.out.printf(">");
        int b = scanner.nextInt();
+       
+       while (b < 0 || b > 100) {
+           System.out.println("잘못 입력했습니다.");
+           System.out.println("B 값을 입력하세요.");
+           System.out.print("> ");
+           b = scanner.nextInt();
+       }
 
        System.out.printf("A의 값은 B의 값");
        if(a > b) {
