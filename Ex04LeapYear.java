027469e68adad9c4f70801f4c7f45cfaebe3fc2c
yunheeversal <98575048+yunheeversal@users.noreply.github.com>
2022-03-08 13:07:05 +0900

    LeapYear 02
    while문 추가해서 재 입력 받기

--- Ex04LeapYear.java
+++ Ex04LeapYear.java
@@ -6,19 +6,24 @@ public class Ex04LeapYear {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("연도를 입력하세요.");
         int year = scanner.nextInt();
-        
-        if(year<1 || year>4000) {
-            System.out.println("연도를 다시 입력하세요");
-        } else if(year%4 == 0 && year % 100 != 0) {
+
+        while (year < 1 || year > 4000) {
+            System.out.println("잘못 입력했습니다.");
+            System.out.println("연도를 입력해 주세요.");
+            System.out.print("> ");
+            year = scanner.nextInt();
+        }
+
+        if (year % 4 == 0 && year % 100 != 0) {
             System.out.println("윤년");
-        } else if( year % 400 == 0) {
+        } else if (year % 400 == 0) {
             System.out.println("윤년");
         } else {
             System.out.println("평년");
-       }
+        }
 
     }
 
